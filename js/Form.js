3b8baf571383a11c1ff8c3bcc147426eeee511bb
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-03-06 11:21:25 -0300

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -3,46 +3,26 @@ class Form {
     this.input = createInput("").attribute("placeholder", "Digite seu nome");
     this.playButton = createButton("Play");
     this.titleImg = createImg("./assets/title.png", "título do jogo");
-    this.greeting = createElement("h2");
-  }
-
+    this.greeting = createElement("h2");        }
   setElementsPosition() {
     this.titleImg.position(120, 100);
     this.input.position(width / 2 - 110, height / 2 - 80);
     this.playButton.position(width / 2 - 90, height / 2 - 20);
-    this.greeting.position(width / 2 - 300, height / 2 - 100);
-  }
-
- /* setElementsStyle() {
-    this.titleImg.class("gameTitle");
-    this.input.class("customInput");
-    this.playButton.class("customButton");
-    this.greeting.class("greeting");
-  } */
-
+    this.greeting.position(width / 2 - 300, height / 2 - 100);        }
   hide() {
     this.greeting.hide();
     this.playButton.hide();
-    this.input.hide();
-  }
-
+    this.input.hide();      }
   handleMousePressed() {
     this.playButton.mousePressed(() => {
       this.input.hide();
       this.playButton.hide();
-      var message = `
-      Olá ${this.input.value()}
-      </br>espere outro jogador entrar...`;
+      var message = ` Olá ${this.input.value()} </br>espere outro jogador entrar...`;
       this.greeting.html(message);
       player.name = this.input.value();
       player.index = 1;
-      //player.addPlayer();
-    });
-  }
-
+      //player.addPlayer();          });             }
   display() {
    this.setElementsPosition();
-    //this.setElementsStyle();
-    this.handleMousePressed();
-  }
+   this.handleMousePressed();     }
 }
